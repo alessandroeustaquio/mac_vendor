6a860c517d4ab770a4845e6d3d28f4d966b50d9b
NetAcad DEVASC <changeme@example.com>
2021-05-15 03:10:20 +0000

    Programa Funcionando

--- client.py
+++ client.py
@@ -1,3 +1,19 @@
-print("Hello would")
-print("New Hello")
-print(" A ")
+from os import linesep
+import requests
+
+def ler_txt():
+    macs = {}
+    with open('MACs.txt', 'r') as fp:
+        lines = fp.readlines()
+        #macs = [line.rstrip() for line in lines]
+        # mesma coisa que acima
+        if lines:
+            for line in lines:
+                line = line.rstrip()
+                line_array = line.split(";")
+                ip=line_array[0]
+                mac=line_array[1]
+                macs[ip]=mac
+    print(line_array)
+    return macs
+print(m)
